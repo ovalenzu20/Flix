de0f12c699dd22ea40eae22049fcf7ed17e9a246
Omar Valenzuela <ovalenzu@uci.edu>
2018-08-10 18:43:44 -0700

    removed comments

--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -94,16 +94,5 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
