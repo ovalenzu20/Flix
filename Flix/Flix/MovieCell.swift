bd82831663de2bac4fdec8b356961eb87f8e119c
Omar Valenzuela <ovalenzu@uci.edu>
2018-08-10 17:49:32 -0700

    Alamofire pod installation and refreshControl implemented

---
+++ Flix/Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Omar Valenzuela on 8/9/18.
+//  Copyright © 2018 Omar Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Flix/Flix/NowPlayingViewController.swift
+++ Flix/Flix/NowPlayingViewController.swift
@@ -7,26 +7,54 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource{
 
+    var movies : [[String : Any]] = []
     @IBOutlet weak var tableView: UITableView!
+    var refreshControl : UIRefreshControl!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        let posterPath = movie["poster_path"] as! String
+        let baseURL = "https://image.tmdb.org/t/p/w500"
+        let posterURL = URL(string: baseURL + posterPath)!
+
+        cell.titleLabel.text! = title
+        cell.overviewLabel.text! = overview
+        cell.posterImage.af_setImage(withURL: posterURL)
         
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.rowHeight = 245
         tableView.dataSource = self
+        tableView.estimatedRowHeight = 245
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        fetchMovies()
+        
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl : UIRefreshControl){
+        fetchMovies()
+    }
+
+    func fetchMovies(){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -37,21 +65,19 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource{
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
                 let movies = dataDictionary["results"] as! [[String : Any]]
-                
-                for movie in movies {
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
             }
         }
         task.resume()
     }
-
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
-        
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 
     /*
     // MARK: - Navigation
